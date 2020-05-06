3e94971e75030f708bed1d340c575dd243dd14d9
Jose Amesquita <joseamesquita11@gmail.com>
2020-05-06 11:30:54 -0700

    adding redux to the application
    Co-authored-by: Hristo Dzhambov <dzhambov@hotmail.com>
    Co-authored-by: Dominique Youmans <46074403+21-Coding@users.noreply.github.com>

--- src/components/SurveyControl.js
+++ src/components/SurveyControl.js
@@ -58,53 +58,84 @@ handleChangingSelectedSurvey = (id) => {
   this.setState({ selectedSurvey: selectedSurvey });
 }
 
-handleChangingSelectedSurvey = (id) => {
-  this.props.firestone.get({ collection: 'surveys', doc: id }).then((survey) => {
-    const firestoreSurvey = {
-      names: survey.get("names"),
-      language: survey.get("language"),
-      framework: survey.get("framework"),
-      id: survey.id
-    }
-    this.setState({ selectedSurvey: firestoreSurvey });
-  });
-}
+// handleChangingSelectedSurvey = (id) => {
+//   this.props.firestone.get({ collection: 'surveys', doc: id }).then((survey) => {
+//     const firestoreSurvey = {
+//       names: survey.get("names"),
+//       language: survey.get("language"),
+//       framework: survey.get("framework"),
+//       id: survey.id
+//     }
+//     this.setState({ selectedSurvey: firestoreSurvey });
+//   });
+// }
 
-handleEditClick = () => {
-  this.setState({ editing: true });
-}
+// handleEditClick = () => {
+//   this.setState({ editing: true });
+// }
 
-handleEditingTicketInList = () => {
-  this.setState({
-    editing: false,
-    selectedSurvey: null
-  });
-}
+// handleEditingTicketInList = () => {
+//   this.setState({
+//     editing: false,
+//     selectedSurvey: null
+//   });
+// }
 
-render() {
-  const auth = this.props.firebase.auth();
-  if (!isLoaded(auth)) {
-    return (
-      <React.Fragment>
-        <h1>Loading...</h1>
-      </React.Fragment>
-    )
-  }
-}
-if ((isLoaded(auth)) & (auth.currentUser == null)) {
-  return (
-    <React.Fragment>
-      <h1>You must be signed in to access the queue.</h1>
-    </React.Fragment>
-  )
-}
-if ((isLoaded(auth)) && (auth.currentUser != null)) {
+render(){
   let currentlyVisibleState = null;
   let buttonText = null;
-  if (this.state.editing) {
 
+  if (this.state.selectedSurvey != null) {
+    currentlyVisibleState =
+      <SurveyDetail
+        survey={this.state.selected}
+        onClickingDelete={this.handleDeletingSurvey}
+        onClickingEdit={this.handleEditClick} />
+    buttonText = "Return to Survey List";
+  } else if (this.state.formVisibleOnPage) {
+    currentlyVisibleState = <NewSurveyForm onNewSurveyicketCreation={this.handleAddiSurveyicketToList} />;
+    buttonText = "ReturSurveyicket List";
+  } else {
+    currentlyVisibleStatSurveyicketsurveyicketList = {
+      this.state.mSurveyicketLisSurveyicketSelection = { this.handleChangingselected } />;
+      buttonText = Surveyicket";
+}
+    return (
+      <React.Fragment>
+        {currentlyVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button>
+      </React.Fragment>
+    );
   }
 
-
 }
+
+export defSurveyicketControl;
+
+// render() {
+//   const auth = this.props.firebase.auth();
+//   if (!isLoaded(auth)) {
+//     return (
+//       <React.Fragment>
+//         <h1>Loading...</h1>
+//       </React.Fragment>
+//     )
+//   }
+// }
+// if ((isLoaded(auth)) & (auth.currentUser == null)) {
+//   return (
+//     <React.Fragment>
+//       <h1>You must be signed in to access the queue.</h1>
+//     </React.Fragment>
+//   )
+// }
+// if ((isLoaded(auth)) && (auth.currentUser != null)) {
+//   let currentlyVisibleState = null;
+//   let buttonText = null;
+//   if (this.state.editing) {
+
+//   }
+
+
+// }
 
