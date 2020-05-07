f945f4b2e7b7ff42d1b53afd2f417bd6d94b74a8
Jose Amesquita <joseamesquita11@gmail.com>
2020-05-07 13:44:55 -0700

    add survey test & reducer test
    Co-authored-by: Hristo Dzhambov <dzhambov@hotmail.com>
    Co-authored-by: Dominique Youmans <46074403+21-Coding@users.noreply.github.com>

--- src/__tests__/reducers/survey-list-reducer.test.js
+++ src/__tests__/reducers/survey-list-reducer.test.js
@@ -2,29 +2,31 @@ import surveyListReducer from '../../reducers/survey-list-reducer';
 import * as c from './../../actions/ActionTypes';
 
 describe('surveyListReducer', () => {
-
+  let action;
+  const surveyData = {
+    1:
+    {
+      question1: 'Do you ride the bus tho work?',
+      response1: 'No',
+      id: 1
+    }
+  };
 
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(surveyListReducer({}, { type: null })).toEqual({});
   });
   test('Should successfully add new survey data to masterSurveyList', () => {
-    const { question1, question2, question3, response1, response2, response3, id } = surveyData;
+    const { question1, response1, id } = surveyData;
     action = {
       type: c.ADD_SURVEY,
       question1: question1,
       response1: response1,
-      question2: question2,
-      response2: response2,
-      question3: response3,
       id: id
     };
     expect(surveyListReducer({}, action)).toEqual({
       [id]: {
         question1: question1,
         response1: response1,
-        question2: question2,
-        response2: response2,
-        question3: response3,
         id: id
       }
     });
--- src/reducers/survey-list-reducer.js
+++ src/reducers/survey-list-reducer.js
@@ -1,3 +1,22 @@
+import * as c from './../actions/ActionTypes';
+
 export default (state = {}, action) => {
-  return state;
+  const { question1, response1, id } = action;
+  switch (action.type) {
+    case c.ADD_SURVEY:
+      return Object.assign({}, state, {
+        [id]: {
+          question1: question1,
+          response1: response1,
+          id: id
+        }
+      });
+    default:
+      return state;
+  }
 };
+
+
+
+
+
