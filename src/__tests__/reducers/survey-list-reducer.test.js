8f53180e33e15b6be36fe4edb1696be8a9189a95
Jose Amesquita <joseamesquita11@gmail.com>
2020-05-07 11:55:03 -0700

    currently working on testing reducers
    Co-authored-by: Hristo Dzhambov <dzhambov@hotmail.com>
    Co-authored-by: Dominique Youmans <46074403+21-Coding@users.noreply.github.com>

--- src/__tests__/reducers/survey-list-reducer.test.js
+++ src/__tests__/reducers/survey-list-reducer.test.js
@@ -1,7 +1,32 @@
 import surveyListReducer from '../../reducers/survey-list-reducer';
+import * as c from './../../actions/ActionTypes';
 
 describe('surveyListReducer', () => {
+
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(surveyListReducer({}, { type: null })).toEqual({});
+  });
+  test('Should successfully add new survey data to masterSurveyList', () => {
+    const { question1, question2, question3, response1, response2, response3, id } = surveyData;
+    action = {
+      type: c.ADD_SURVEY,
+      question1: question1,
+      response1: response1,
+      question2: question2,
+      response2: response2,
+      question3: response3,
+      id: id
+    };
+    expect(surveyListReducer({}, action)).toEqual({
+      [id]: {
+        question1: question1,
+        response1: response1,
+        question2: question2,
+        response2: response2,
+        question3: response3,
+        id: id
+      }
+    });
   });
 });
